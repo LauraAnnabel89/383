0d83fa104d3404d420b8e5501ad24d3a7b82dafe
Laura Tombs <laura.tombs@asglh-ml-14142.local>
2018-09-23 09:41:59 +0100

    one post is displaying on the page

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,34 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://private-cc77e-aff.apiary-mock.com/posts")
+      .then(d => d.json())
+      .then((d, id) => {
+        let posts = d.items.map(post => {
+          this.setState({ posts: post });
+        });
+        console.log(this.state.posts);
+      });
+  }
+
   render() {
-    return <div className="App" />;
+    let posts = this.state.posts;
+    return (
+      <ul>
+        <div key={posts.item_id}>
+          <h2>{posts.item_created}</h2>
+          <p>{posts.item_source_id}</p>
+        </div>
+      </ul>
+    );
   }
 }
 
